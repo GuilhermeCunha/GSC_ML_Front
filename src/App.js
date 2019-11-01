0f9f68a5b394a31130baec21a93fc59b1721473d
GuilhermeCunha <guiscunha@gmail.com>
2019-10-31 23:53:01 -0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './services/api';
+import api from './services/api'
+
+
+//_Layout CSS
+import './pages/res/css/_layout.css'
+import './pages/Login/login.css'
+require('dotenv/config');
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ 
+
+  async function handleSubmit(event){
+    event.preventDefault();
+
+    const response = await api.post('/login', {
+      email: email,
+      password: password
+    });
+    console.log(response);
+    console.log("Clicou");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{process.env.REACT_API_BASE_UR}</h1>
+        <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div className="card card-signin my-5">
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Login</h5>
+                        <form className="form-signin" onSubmit={handleSubmit}>
+                            <div className="form-label-group">
+                                <input type="email" 
+                                id="email" 
+                                className="form-control" 
+                                placeholder="Email address"
+                                value={email}
+                                onChange={event => setEmail(event.target.value)} 
+                                required 
+                                autoFocus 
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+
+                            <div className="form-label-group">
+                                <input type="password" 
+                                id="password" 
+                                className="form-control" 
+                                placeholder="Password"
+                                value={password}
+                                onChange={event => setPassword(event.target.value)} 
+                                required />
+                                <label htmlFor="password">Senha</label>
+                            </div>
+                            <button id="btnLogin" className="btn btn-lg btn-primary btn-block text-uppercase"
+                                type="submit">Login</button>
+                            <hr/>
+
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="link text-center">
+                                            Ainda não tem conta?
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 text-center">
+                                        <a className="" href="#paracadastro">Cadastre-se</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+//_Layout CSS
+import './res/css/_layout.css'
+
+function Login() {
+  return (
+    <div className="App">
+      <h1>OK</h1>
+    </div>
+  );
+}
+
+export default Login;
