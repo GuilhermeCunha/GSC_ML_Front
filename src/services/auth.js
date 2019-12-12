a54633650d9e5b107f2907d0ab3274522af101a4
GuilhermeCunha <guiscunha@gmail.com>
2019-12-11 22:36:18 -0300

    Roda ADMIN

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,10 +2,10 @@ export const TOKEN_KEY = "@token";
 export const EMAIL_KEY = "@email"
 export const NICKNAME_KEY = "@nickname"
 export const ID_KEY = "@id"
-
 export const ISADMIN_KEY = "@isAdmin"
+
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
-export const isAdmin = () => localStorage.getItem(ISADMIN_KEY === 's');
+export const isAdmin = () => localStorage.getItem(ISADMIN_KEY) === 's';
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = data => {
   localStorage.setItem(TOKEN_KEY, data.token);
