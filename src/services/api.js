d30b4037b003a133f922e4bb64128395ad199953
GuilhermeCunha <guiscunha@gmail.com>
2019-12-17 14:20:15 -0300

    Axios

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { getToken } from "./auth";
 require('dotenv').config();
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL  
 });
 
 api.interceptors.request.use(async config => {
